c277cefcca9bc62dccedf3e4866c544c68acd0e2
UltraPokemosha <sfygithub@gmail.com>
2024-08-30 00:30:44 +0300

    fix: u title injected class has smaller cascading lvl

--- src/components/ui/u-title/UTitle.vue
+++ src/components/ui/u-title/UTitle.vue
@@ -7,33 +7,31 @@ const props = defineProps<Props>()
 </script>
 
 <template>
-  <component :is="`${props.tag}`" :class="[`_${props.tag}`, 'u-title']">
+  <component :is="`${props.tag}`" :class="[`_${props.tag}`]">
     <slot></slot>
   </component>
 </template>
 
 <style lang="scss" scoped>
-.u-title {
-  &._h1 {
-    @extend .text-48;
-    color: $secondary-color;
-    font-family: $secondary-font;
-    text-align: center;
-  }
-  &._h2 {
-    @extend .text-36;
-  }
-  &._h3 {
-    @extend .text-32;
-  }
-  &._h4 {
-    @extend .text-24;
-  }
-  &._h5 {
-    @extend .text-20;
-  }
-  &._h6 {
-    @extend .text-18;
-  }
+._h1 {
+  @extend .text-48;
+  color: $secondary-color;
+  font-family: $secondary-font;
+  text-align: center;
+}
+._h2 {
+  @extend .text-36;
+}
+._h3 {
+  @extend .text-32;
+}
+._h4 {
+  @extend .text-24;
+}
+._h5 {
+  @extend .text-20;
+}
+._h6 {
+  @extend .text-18;
 }
 </style>
