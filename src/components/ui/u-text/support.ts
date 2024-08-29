2419bb90ca3bfc17a27a81c7b2cdcfeeed3cc9f7
UltraPokemosha <sfygithub@gmail.com>
2024-08-29 18:11:55 +0300

    change: size type

--- src/components/ui/u-text/support.ts
+++ src/components/ui/u-text/support.ts
@@ -1,5 +1,5 @@
 export type TextWeight = "m" | "sb" | "b"
-export type TextSize = 12 | 14 | 16 | 18 | 20 | 24 | 30 | 36 | 48 | 60 | 72 | 96 | 128
+export type TextSize = 12 | 14 | 16 | 18 | 20 | 24 | 30 | 32 | 36 | 48 | 60 | 72 | 96 | 128
 
 export interface ITextConfig {
   size: TextSize
