49fd237170998b015db297798439fffbf39e1dce
UltraPokemosha <sfygithub@gmail.com>
2024-08-29 18:13:03 +0300

    add: uradio component

---
+++ src/components/ui/u-radio/URadio.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+import { computed } from "vue";
+import type { URadioProps } from "./support";
+
+const props = defineProps<URadioProps>()
+
+const emits = defineEmits<{
+  "update:value": [string, string]
+}>()
+
+function onChange(e: Event) {
+  const input = e.target as HTMLInputElement
+  emits("update:value", input.value, input.id)
+}
+
+const listener = computed(() => (props.needListener ? "change" : ""))
+
+const isChecked = computed(() => props['checked'] ? props.checked: false)
+</script>
+
+<template>
+  <input
+    v-on:[listener]="onChange"
+    :class="['u-radio__input', props.inputClass || '']"
+    type="radio"
+    :name="props.name"
+    :id="props.id"
+    :checked="isChecked"
+    :value="props.value"
+  />
+  <label :class="['u-radio', props.labelClass ?? '']" :for="props.id">
+    <slot></slot>
+  </label>
+</template>
+
+<style lang="scss" scoped>
+.u-radio {
+  cursor: pointer;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  gap: $normal-gap;
+
+  &__input {
+    display: none;
+  }
+}
+</style>
---
+++ src/components/ui/u-radio/support.ts
@@ -0,0 +1,9 @@
+export interface URadioProps {
+  id: string
+  name: string
+  value: string
+  needListener: boolean
+  checked?: boolean
+  inputClass?: string
+  labelClass?: string
+}
